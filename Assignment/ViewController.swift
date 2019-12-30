7736c47b03ab2dde46331d48b79ec12068644cfa
Madhuri Satpute <madhuri.satpute@orangebitsindia.com>
2019-12-30 15:19:08 +0530

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Prakash Sabale on 30/12/19.
+//  Copyright © 2019 Orangebit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
